86efbd3d1279a6db5a56be6b260b7fe86fd3a395
Emanuele <emanuele65b@gmail.com>
2021-06-01 16:58:09 +0200

    add parola corta

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
-// Prompt
-var number = prompt ();
-var number = prompt ();
-var number = prompt ();
-var number = prompt ();
-var number = prompt ();
-var number = prompt ();
+// Parola corta
+var wordfirst = prompt ('Inserisci una parola casuale');
+var wordsecond = prompt ('Inserisci una parola casuale');
+
+var longfirst = wordfirst.length;
+var longsecond = wordsecond.length;
+
+document.getElementById('parola').innerHTML = wordfirst + wordsecond;
+
 
 // Numero dispari
+var number = prompt ('Inserisci un numero');
+var number = prompt ('Inserisci un numero');
+var number = prompt ('Inserisci un numero');
+var number = prompt ('Inserisci un numero');
+var number = prompt ('Inserisci un numero');
+var number = prompt ('Inserisci un numero');
+
 if (number % 2 == 0){
     console.log ("This number is even.");
 }
